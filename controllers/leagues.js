77b0530959cf0b5a6e6b65f1c47b392e71ee529a
andrewbantly <andrewbantly@gmail.com>
2023-04-09 17:04:50 -0700

    picks are successfully loaded into database

--- controllers/leagues.js
+++ controllers/leagues.js
@@ -9,11 +9,33 @@ router.get("/", (req, res) => {
     axios.get(mlbURL).then(apiResponse => {
         let sports = apiResponse.data;
         // console.log(sports.events[14].competitions[0].competitors[0].team.logo);
-        console.log(sports.events[3]);
+        // console.log("short name: ", sports.events[14]);
+    console.log("res locals ID: ", res.locals.user.id); 
+        const userId = res.locals.user.id;      
         res.render("leagues/index.ejs", {
-            sports
+            sports, userId
         })
     })
+})
+
+router.post("/", async (req, res) => {
+    console.log("req body: ", req.body);
+    try {
+        const newPick = await db.pick.create({
+            userId: req.body.userId,
+            league: req.body.league,
+            game: req.body.game,
+            selectedTeam: req.body.selectedTeam,
+            gameDate: req.body.gameDate,
+            shortName: req.body.shortName,
+            gameStatus: req.body.gameStatus,
+            pickActive: req.body.pickActive,
+        })
+        res.redirect("/leagues")
+    } catch (error) {
+        console.log(error);
+        res.redirect("404.ejs")
+    }
 })
 
 
